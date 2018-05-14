cf26d397090880ebf89b81162afa6c176f30a223
tbhesswebber <tbhesswebber@gmail.com>
2018-05-14 17:12:37 -0400

    prelim passport and session setup

--- server/middleware/sessions.js
+++ server/middleware/sessions.js
@@ -1,7 +1,7 @@
 expressSessionSettings = {
   secret: process.env.SESSION_SECRET || "I_am_a_secret",
-  resave: true,
-  saveUninitialized: true
+  resave: false, // only saves on update
+  saveUninitialized: false // only saves on login
 };
 
 export default expressSessionSettings;
