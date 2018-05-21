e25730993fe590b6a3a8bfdc0becc4d971a66eb1
Leonard Chung <leonardjchung@gmail.com>
2018-05-21 19:29:24 -0400

    partial implementation of auto carousel in DataTypes page

--- client/src/components/DataTypes.jsx
+++ client/src/components/DataTypes.jsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import Slider from "react-slick";
 import "./slick/slick.css";
 import "./slick/slick-theme.css";
+import "./DataTypes.css";
 
 
 class DataTypes extends Component {
@@ -15,19 +16,82 @@ class DataTypes extends Component {
   }
 
   render() {
+    // const settings = {
+    //   dots: true,
+    //   infinite: true,
+    //   speed: 500,
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1
+    // };
+    // return (
+    //   <div className="carousel">
+    //     <h2 className="dt-heading"> Single Item</h2>
+    //     <Slider {...settings}>
+    //       <div>
+    //         <h3>1</h3>
+    //       </div>
+    //       <div>
+    //         <h3>2</h3>
+    //       </div>
+    //       <div>
+    //         <h3>3</h3>
+    //       </div>
+    //       <div>
+    //         <h3>4</h3>
+    //       </div>
+    //       <div>
+    //         <h3>5</h3>
+    //       </div>
+    //       <div>
+    //         <h3>6</h3>
+    //       </div>
+    //     </Slider>
+    //   </div>
+    // );
+
+    // const settings = {
+    //   slidesToShow: 3,
+    //   slidesToScroll: 1,
+    //   autoplay: true,
+    //   autoplaySpeed: 2000,
+    //   dots: true,
+    //   infinite: true,
+    //   speed: 500,
+    //   fade: true,
+    //   cssEase: 'linear'
+    // };
+
+    // return (
+    //   <div className="carousel">
+    //     <h2 className="dt-heading"> Data Types</h2>
+    //       <Slider {...settings}>
+    //         <div>
+    //           <div>
+    //             <h3>1</h3>
+    //           </div>
+    //           <div>2</div>
+    //           <div>2</div>
+    //         </div>
+    //       </Slider>
+    //   </div>
+    // );
+
     const settings = {
       dots: true,
       infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 2000,
+      cssEase: "linear"
     };
     return (
-      <div>
-        <h2> Single Item</h2>
+      <div className="carousel">
+        <h2 className="carousel-heading">Auto Play</h2>
         <Slider {...settings}>
           <div>
-            <h3>1</h3>
+            <h3><img className="card-1" alt="1" src="img/wolf.png"></img></h3>
           </div>
           <div>
             <h3>2</h3>
@@ -47,28 +111,6 @@ class DataTypes extends Component {
         </Slider>
       </div>
     );
-
-    // const settings = {
-    //   slidesToShow: 1, // 3
-    //   slidesToScroll: 1,
-    //   autoplay: true,
-    //   autoplaySpeed: 2000,
-    //   dots: true,
-    //   infinite: true,
-    //   speed: 500,
-    //   fade: true,
-    //   cssEase: 'linear'
-    // };
-
-    // return (
-    //   <Slider {...settings}>
-    //     <div>
-    //       <div>1</div>
-    //       <div>2</div>
-    //       <div>2</div>
-    //     </div>
-    //   </Slider>
-    // );
   }
 }
 
