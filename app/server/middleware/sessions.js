c2c6a429beadba1accda596fd282a8ea32287fbb
tbhesswebber <tbhesswebber@gmail.com>
2018-05-23 19:11:49 -0400

    updates for process.env

--- app/server/middleware/sessions.js
+++ app/server/middleware/sessions.js
@@ -2,7 +2,7 @@
 import { SESSION_SECRET } from '../../../config';
 
 const expressSessionSettings = {
-  secret: SESSION_SECRET || "I_am_a_secret",
+  secret: process.env.SESSION_SECRET || "I_am_a_secret",
   resave: false, // only saves on update
   saveUninitialized: false // only saves on login
 };
