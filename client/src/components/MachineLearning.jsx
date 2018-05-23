6462da6f6b61c53d7ddc28258f2a1b242b44776e
Leonard Chung <leonardjchung@gmail.com>
2018-05-22 20:25:46 -0400

    Basic layout of examples section of Machine Learning page

--- client/src/components/MachineLearning.jsx
+++ client/src/components/MachineLearning.jsx
@@ -32,89 +32,53 @@ const MachineLearning = (props) => {
               </div>
           </div>
       </div>
-      <div className="section-features">
+      <div className="section-features-ml">
           <div className="section-features__bg">
-              <img className="section-features__bg__pic" src="img/data.jpg" alt="data">
-              </img>
-          </div>
-          <div className="row">
-              <div className="col-1-of-4">
-                  <div className="feature-box">
-                      <h3 className="heading-tertiary margin-bottom-small">Explore your data</h3>
-                      <p className="feature-box__text">
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
-                      </p>
-                  </div>
-              </div>
-              <div className="col-1-of-4">
-                  <div className="feature-box">
-                      <h3 className="heading-tertiary margin-bottom-small">Save time</h3>
-                      <p className="feature-box__text">
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
-                      </p>
-                  </div>
-              </div>
-              <div className="col-1-of-4">
-                  <div className="feature-box">
-                      <h3 className="heading-tertiary margin-bottom-small">Combine your data</h3>
-                          <p className="feature-box__text">
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
-                      </p>
-                  </div>
-              </div>
-              <div className="col-1-of-4">
-                  <div className="feature-box">
-                      <h3 className="heading-tertiary margin-bottom-small">Versatile support</h3>
-                      <p className="feature-box__text">
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
-                      </p>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <section className="section-stories">
-          <div className="section-stories__bg">
-              <img className="section-stories__bg__pic" src="img/abstract_ai.jpg" alt="bulb">
+              <img className="section-features__bg__pic" src="img/ai-android.jpg" alt="ai android">
               </img>
           </div>
           <div className="center-text margin-bottom-big">
               <h2 className="heading-secondary">
-                  See what people are saying about SimplifAi
+                  People use machine learning every day
               </h2>
           </div>
-          <div className="row">
-              <div className="story">
-                  <figure className="story__shape">
-                      <img src="img/Cody.png" alt="Cody" className="story__img"/>
-                      <figcaption className="story__caption">Cody Daig</figcaption>
-                  </figure>
-                  <div className="story__text">
-                      <h3 className="heading-tertiary margin-bottom-small">I had a great time using SimplifAi</h3>
-                      <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
-                          ducimus quam nisi exercitationem omnis earum qui. Aperiam, ipsum sapiente aspernatur libero
-                          repellat quis consequatur ducimus quam nisi exercitationem omnis.
+          <div className="grid-ml">
+              <div className="col-1-of-4">
+                  <div className="feature-box-ml">
+                    {/* <img ></img> */}
+                  </div>
+                  <h3 className="heading-tertiary-ml margin-bottom-small margin-top-small">Explore your data</h3>
+                      <p className="feature-box__text">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
                       </p>
+              </div>
+              <div className="col-1-of-4">
+                  <div className="feature-box-ml">
+                      {/* <h3 className="heading-tertiary margin-bottom-small">Save time</h3>
+                      <p className="feature-box__text">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
+                      </p> */}
+                  </div>
+              </div>
+              <div className="col-1-of-4">
+                  <div className="feature-box-ml">
+                      {/* <h3 className="heading-tertiary margin-bottom-small">Combine your data</h3>
+                          <p className="feature-box__text">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
+                      </p> */}
+                  </div>
+              </div>
+              <div className="col-1-of-4">
+                  <div className="feature-box-ml">
+                      {/* <h3 className="heading-tertiary margin-bottom-small">Versatile support</h3>
+                      <p className="feature-box__text">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
+                      </p> */}
                   </div>
               </div>
           </div>
-          <div className="row">
-              <div className="story">
-                  <figure className="story__shape">
-                      <img src="img/Ralph.png" alt="Ralph" className="story__img"/>
-                      <figcaption className="story__caption">Ralph Plumley</figcaption>
-                  </figure>
-                  <div className="story__text">
-                      <h3 className="heading-tertiary margin-bottom-small">SimplifAi really changed my life</h3>
-                      <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
-                          ducimus quam nisi exercitationem omnis earum qui. Aperiam, ipsum sapiente aspernatur libero
-                          repellat quis consequatur ducimus quam nisi exercitationem omnis.
-                      </p>
-                  </div>
-              </div>
-          </div>
-      </section>
+      </div>
+      
     </div>
   );
 }
