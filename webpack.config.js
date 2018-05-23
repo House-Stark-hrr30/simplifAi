94335b69bf2616bed112d1c9c3175b625f2754d9
tbhesswebber <tbhesswebber@gmail.com>
2018-05-23 14:21:03 -0400

    corrections to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('path');
-// const autoprefixer = require('autoprefixer');
 
-const SRC_DIR = path.join(__dirname, 'client/src');
-const DIST_DIR = path.join(__dirname, 'client/build');
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const DIST_DIR = path.join(__dirname, 'client','build');
 
 module.exports = {
   entry: `${SRC_DIR}/index.js`,
@@ -22,50 +21,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: 'style-loader'
+        loaders: ["style-loader", 'css-loader'],
+        include : SRC_DIR
       },
       {
-        test: /\.css$/,
-        loader: 'css-loader',
-        query: {
-          modules: true,
-          localIdentName: '[name]__[local]___[hash:base64:5]'
-        }
+        test: /\.css$/,  
+        include: /node_modules/,  
+        loaders: ['style-loader', 'css-loader'],
       }
-      // {
-        
-      //   test: /\.css$/,
-      //   include: SRC_DIR,
-      //   use: [
-      //     require.resolve('style-loader'),
-      //     {
-      //       loader: require.resolve('css-loader'),
-      //       options: {
-      //         importLoaders: 1,
-      //       },
-      //     },
-      //     {
-      //       loader: require.resolve('postcss-loader'),
-      //       options: {
-      //         // Necessary for external CSS imports to work
-      //         // https://github.com/facebookincubator/create-react-app/issues/2677
-      //         ident: 'postcss',
-      //         plugins: () => [
-      //           require('postcss-flexbugs-fixes'),
-      //           autoprefixer({
-      //             browsers: [
-      //               '>1%',
-      //               'last 4 versions',
-      //               'Firefox ESR',
-      //               'not ie < 9', // React doesn't support IE8 anyway
-      //             ],
-      //             flexbox: 'no-2009',
-      //           }),
-      //         ],
-      //       },
-      //     },
-      //   ],  
-      // }
     ]
   }
 };
